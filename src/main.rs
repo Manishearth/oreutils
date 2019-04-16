ded5024f33311251236a4dcf4ac76f158fce946c
Manish Goregaokar <manishsmail@gmail.com>
2019-04-15 21:10:57 -0700

    Support upgrading, kind of

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+use semver::Version;
 use std::process::Command;
 use structopt::StructOpt;
 
@@ -72,7 +74,45 @@ fn install() {
 }
 
 fn upgrade() {
-    unimplemented!()
+    for tool in TOOLS {
+        let res = upgrade_tool(tool);
+        match res {
+            Ok(vers) => println!("Tool {} updated from version {}", tool.name, vers),
+            Err(Error::NotFound) => println!(
+                "Tool {} not installed, use `oreutils install` to install",
+                tool.name
+            ),
+            Err(Error::VersionBroken(None)) => {
+                println!("`{} --version` didn't produce expected output", tool.cli)
+            }
+            Err(Error::VersionBroken(Some(v))) => println!(
+                "`{} --version` didn't produce expected output: could not parse {}",
+                tool.cli, v
+            ),
+        }
+    }
+}
+
+enum Error {
+    NotFound,
+    VersionBroken(Option<String>),
+}
+
+fn upgrade_tool(tool: &Tool) -> Result<Version, Error> {
+    let output = Command::new(tool.cli)
+        .args(&["--version"])
+        .output()
+        .map_err(|_| Error::NotFound)?;
+    let output = String::from_utf8(output.stdout).map_err(|_| Error::VersionBroken(None))?;
+    let output = output.lines().next().ok_or(Error::VersionBroken(None))?;
+    let re = Regex::new(r"\d+\.\d+\.\d+").unwrap();
+    let vers = re
+        .find(output)
+        .ok_or(Error::VersionBroken(Some(output.into())))?;
+    let vers = vers.as_str();
+    let vers = Version::parse(vers).map_err(|_| Error::VersionBroken(Some(vers.into())))?;
+    cargo_install(tool.package, true);
+    Ok(vers)
 }
 
 fn uninstall() {
