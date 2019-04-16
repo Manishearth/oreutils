d22b55518e7b4507b43215ba72d35d845df89963
Manish Goregaokar <manishsmail@gmail.com>
2019-04-15 20:45:15 -0700

    Implement installation

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,74 @@
+use std::process::Command;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "oreutils", about = "Installation manager for various CLI utilities reimagined in Rust", rename_all = "kebab-case")]
+#[structopt(
+    name = "oreutils",
+    about = "Installation manager for various CLI utilities reimagined in Rust",
+    rename_all = "kebab-case"
+)]
 enum Opt {
     #[structopt(about = "Install the basic utilities: ripgrep, exa, bat, fd")]
     Install,
-    #[structopt(about = "Upgrade any installed tools. Use `oreutils install` to install missing ones.")]
+    #[structopt(
+        about = "Upgrade any installed tools. Use `oreutils install` to install missing ones."
+    )]
     Upgrade,
     #[structopt(about = "Uninstall all oreutils tools")]
     Uninstall,
 }
 
 struct Tool {
+    name: &'static str,
     package: &'static str,
     cli: &'static str,
 }
 
-
 const TOOLS: &[Tool] = &[
-    Tool {package: "ripgrep", cli: "rg"},
-    Tool {package: "exa", cli: "exa"},
-    Tool {package: "bat", cli: "bat"},
-    Tool {package: "fd-find", cli: "fd"},
+    Tool {
+        name: "ripgrep",
+        package: "ripgrep",
+        cli: "rg",
+    },
+    Tool {
+        name: "exa",
+        package: "exa",
+        cli: "exa",
+    },
+    Tool {
+        name: "bat",
+        package: "bat",
+        cli: "bat",
+    },
+    Tool {
+        name: "fd",
+        package: "fd-find",
+        cli: "fd",
+    },
 ];
 
 fn main() {
     let opt = Opt::from_args();
 
     match opt {
-        Opt::Install => {
-            install()
-        }
-        Opt::Upgrade => {
-            upgrade()
-        }
-        Opt::Uninstall => {
-            uninstall()
-        }
+        Opt::Install => install(),
+        Opt::Upgrade => upgrade(),
+        Opt::Uninstall => uninstall(),
     }
 }
 
-
 fn install() {
-    unimplemented!()
+    for tool in TOOLS {
+        let exists = which::which(tool.cli);
+        if exists.is_ok() {
+            println!(
+                "Tool {:?} already installed, use `oreutils upgrade` to upgrade",
+                tool.name
+            );
+            continue;
+        }
+        cargo_install(tool.package, false);
+    }
 }
 
 fn upgrade() {
@@ -53,3 +79,16 @@ fn uninstall() {
     unimplemented!()
 }
 
+fn cargo_install(pkg: &str, force: bool) {
+    let mut cmd = Command::new("cargo");
+    if force {
+        cmd.args(&["install", "-f", pkg]);
+    } else {
+        cmd.args(&["install", pkg]);
+    }
+    cmd.env("RUSTFLAGS", "-Ctarget-cpu=native");
+    let res = cmd.spawn();
+    if res.is_err() {
+        eprintln!("Installing {:?} failed", pkg);
+    }
+}
